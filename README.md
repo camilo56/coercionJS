160e7d1d4d4681881075101d845aaf6155556b6d
t_coloc <camilo.colorado@CO-IT005544.local>
2019-03-23 17:36:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Coercion JS
+
+## Step 1: Types [examples](/types.js) 
+* string 
+* number
+* boolean
+* undefined
+### Could be considered as a type as well
+* null
+* function
---
+++ types.js
@@ -0,0 +1,12 @@
+typeof "1991";        // "string"
+typeof 1991;          // "number"
+typeof true;          // "boolean"
+typeof milo;          // "undefined"
+typeof null;          // "object" ???? in the end, you are going to know why ;)
+typeof function(){};  // "function"
+
+
+//the outcome of 'typeof' is a string with the name of the type, that is tricky and some times it is use in interviews
+typeof typeof 1991;   // "string"
+typeof typeof false;  // "string"
+typeof typeof null;   // "string"
